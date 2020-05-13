3735b2cf18ac362c270bf8f5c675343123decd86
Elena <elenaqy@hotmail.com>
2020-05-13 16:55:34 +0200

    Variable entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongoDB:WPsaW3snKROP4wvY@cluster0-3x8fc.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
